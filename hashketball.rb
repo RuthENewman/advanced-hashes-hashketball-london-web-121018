6f20a58b65ad87562e8a1ffb767c4b90d412aa52
ruthnewman <ruthnewman@gmail.com>
2018-12-04 18:23:04 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,5 +1,39 @@
 # Write your code here!
-
+def game_hash 
+  game_hash = {
+  :home => {
+    :team_name => "Brooklyn Nets"
+    :colors => ["Black", "White"],
+    :players => [{
+        :name => "Alan Anderson",
+        :number => 0,
+        :shoe => 16,
+        :points => 22,
+        :rebounds => 12,
+        :assists => 12,
+        :steals => 3,
+        :blocks => 1,
+        :slam_dunks => 1 
+      }, 
+      {
+        :name => "Reggie Evans",
+        :number => 30,
+        :shoe => 14,
+        :points => 12,
+        :rebounds => 12,
+        :assists => 12,
+        :steals => 12,
+        :blocks => 12,
+        :slam_dunks => 7
+      }, 
+      { 
+        :name =>
+      }
+      ]
+    }
+  },
+  :away => {}
+}
 
 
 
